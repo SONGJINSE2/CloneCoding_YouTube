6cf4773ba374787f33406fa96b71be7165947a41
송진세 <open8137@gmail.com>
2022-10-06 21:50:28 +0900

    study

--- clone-init/src/Content.js
+++ clone-init/src/Content.js
@@ -1,30 +1,128 @@
-import React from 'react';
-
+import React from "react";
 
 const Content = () => {
-    return(
-        <div>
-            <div class="container">
-                <div class="contents_btn">
-                    <button onClick='#'>전체</button>
-                    <button onClick='#'>음악</button>
-                    <button onClick='#'>게임</button>
-                    <button onClick='#'>믹스</button>
-                    <button onClick='#'>실시간</button>
-                    <button onClick='#'>사이클링</button>
-                    <button onClick='#'>피트니스</button>
-                    <button onClick='#'>만화 영화</button>
-                    <button onClick='#'>액션 어드벤처 게임</button>
-                    <button onClick='#'>요리</button>
-                    <button onClick='#'>축구</button>
-                    <button onClick='#'>최근에 업로드된 동영상</button>
-                </div>
-
-                
-            </div>
+  return (
+    <div>
+      <div class="container">
+        <div class="contents_btn">
+          <button onClick="#">전체</button>
+          <button onClick="#">음악</button>
+          <button onClick="#">게임</button>
+          <button onClick="#">믹스</button>
+          <button onClick="#">실시간</button>
+          <button onClick="#">사이클링</button>
+          <button onClick="#">피트니스</button>
+          <button onClick="#">만화 영화</button>
+          <button onClick="#">액션 어드벤처 게임</button>
+          <button onClick="#">요리</button>
+          <button onClick="#">축구</button>
+          <button onClick="#">최근에 업로드된 동영상</button>
         </div>
-    )
-}
-
+        <hr />
+        <div class="image">
+          <div class="image1">
+            <div class="content1">
+              <img class="content2" src="../public/img/1.png" />
+              <div class="content3">sss</div>
+            </div>
+            <div class="content1">
+              <img class="content2" src="../public/img/1.png" />
+              <div class="content3">sss</div>
+            </div>
+            <div class="content1">
+              <img class="content2" src="../public/img/1.png" />
+              <div class="content3">sss</div>
+            </div>
+            <div class="content1">
+              <img class="content2" src="../public/img/1.png" />
+              <div class="content3">sss</div>
+            </div>
+          </div>
+        </div>
+        <div class="image">
+          <div class="image1">
+            <div class="content1">
+              <img class="content2" src="../public/img/1.png" />
+              <div class="content3">sss</div>
+            </div>
+            <div class="content1">
+              <img class="content2" src="../public/img/1.png" />
+              <div class="content3">sss</div>
+            </div>
+            <div class="content1">
+              <img class="content2" src="../public/img/1.png" />
+              <div class="content3">sss</div>
+            </div>
+            <div class="content1">
+              <img class="content2" src="../public/img/1.png" />
+              <div class="content3">sss</div>
+            </div>
+          </div>
+        </div>
+        <div class="image">
+          <div class="image1">
+            <div class="content1">
+              <img class="content2" src="../public/img/1.png" />
+              <div class="content3">sss</div>
+            </div>
+            <div class="content1">
+              <img class="content2" src="../public/img/1.png" />
+              <div class="content3">sss</div>
+            </div>
+            <div class="content1">
+              <img class="content2" src="../public/img/1.png" />
+              <div class="content3">sss</div>
+            </div>
+            <div class="content1">
+              <img class="content2" src="../public/img/1.png" />
+              <div class="content3">sss</div>
+            </div>
+          </div>
+        </div>
+        <div class="image">
+          <div class="image1">
+            <div class="content1">
+              <img class="content2" src="../public/img/1.png" />
+              <div class="content3">sss</div>
+            </div>
+            <div class="content1">
+              <img class="content2" src="../public/img/1.png" />
+              <div class="content3">sss</div>
+            </div>
+            <div class="content1">
+              <img class="content2" src="../public/img/1.png" />
+              <div class="content3">sss</div>
+            </div>
+            <div class="content1">
+              <img class="content2" src="../public/img/1.png" />
+              <div class="content3">sss</div>
+            </div>
+          </div>
+        </div>
+        <div class="image">
+          <div class="image1">
+            <div class="content1">
+              <img class="content2" src="../public/img/1.png" />
+              <div class="content3">sss</div>
+            </div>
+            <div class="content1">
+              <img class="content2" src="../public/img/1.png" />
+              <div class="content3">sss</div>
+            </div>
+            <div class="content1">
+              <img class="content2" src="../public/img/1.png" />
+              <div class="content3">sss</div>
+            </div>
+            <div class="content1">
+              <img class="content2" src="../public/img/1.png" />
+              <div class="content3">sss</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr />
+    </div>
+  );
+};
 
 export default Content;
