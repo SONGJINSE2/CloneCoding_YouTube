d6fe1a08e1f7fb15ac407f0db651afa2eb8a1020
송진세 <open8137@gmail.com>
2022-10-09 21:17:38 +0900

    frame/modfiy

--- youtube-practice/src/VideoPlayer/VideoPlayer.jsx
+++ youtube-practice/src/VideoPlayer/VideoPlayer.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import './VideoPlayer.css'
+import React from "react";
+import "./VideoPlayer.css";
 
 const VideoPlayer = (props) => {
-    const url = `https://youtube.com/embed/${props.videoId}`;
-    return (
-    <div className="video-player-wrap">
-        <iframe src={url} title={props.videoId} className="video-player" />
+  const url = `https://youtube.com/embed/${props.videoId}`;
+  return (
+    <div>
+      <div className="video-player-wrap">
+        <div className="video-player-wrap2">
+          <iframe src={url} title={props.videoId} className="video-player" />
+        </div>
+        <div className="video-player-wrap3">
+          <div className="video-player-wrap3"></div>
+        </div>
+      </div>
     </div>
-    );
-}
+  );
+};
 
 export default VideoPlayer;
